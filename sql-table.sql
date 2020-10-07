f65b0a550b026965333e52c4f3032cdecfbe8422
laxmibathula <laxmi.bathula4@gmail.com>
2020-10-07 16:20:09 -0400

    added sql-table.sql file

---
+++ sql-table.sql
@@ -0,0 +1,9 @@
+
+  Creating a Table
+  --------------------
+CREATE TABLE customers (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    address VARCHAR(255),
+    Email VARCHAR(225),
+    password VARCHAR(225));
