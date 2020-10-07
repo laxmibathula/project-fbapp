a622ff6c012e2084a4cd492a340e31750d31f258
laxmibathula <laxmi.bathula4@gmail.com>
2020-10-07 17:23:45 -0400

    removed  http link in logout

--- sql-table.sql
+++ sql-table.sql
@@ -1,9 +1,10 @@
 
-  Creating a Table
-  --------------------
+  -- Creating a Table
+
 CREATE TABLE customers (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     address VARCHAR(255),
     Email VARCHAR(225),
-    password VARCHAR(225));
+    password VARCHAR(225)
+);
